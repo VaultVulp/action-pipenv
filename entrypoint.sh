128c7785f455c629f2e852a0a67be7690abf5510
Pavel Alimpiev <VaultVulp@users.noreply.github.com>
2019-05-14 16:30:45 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 echo "#########################################"
 echo "Starting ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
 
-sh -c "pipenv $*"
+sh -c "pipenv install -d && pipenv $*"
 
 echo "#########################################"
 echo "Completed ${GITHUB_WORKFLOW}:${GITHUB_ACTION}"
